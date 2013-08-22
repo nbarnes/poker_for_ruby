67f4ed0e6525da023f191caa3ab1436d8e2832b3
NBarnes <nbarnes@gmail.com>
2013-08-21 17:49:21 -0700

    Card and CardSpec complete, first test in HandSpec green

--- spec/Card_spec.rb
+++ spec/Card_spec.rb
@@ -1,10 +1,27 @@
 require_relative '../Card'
 
-describe Card, "Card" do
+describe Card, "" do
+
+	it "Should reject non-two-character raw Card constructor" do
+		expect{Card.new("too_long")}.to raise_error("Raw card length not 2 in Card.intialize: too_long")
+	end
+
+	it "Should reject invaid rank tags" do
+		expect{Card.new("1d")}.to raise_error("Unrecognized rank tag in Card.rawRankTagToRank: 1")
+	end
+
+	it "Should reject invaid suit tags" do
+		expect{Card.new("2t")}.to raise_error("Unrecognized suit tag in Card.rawSuitTagToSuit: t")
+	end
 
 	it "Should import and export raw card strings correctly" do
 		card = Card.new("8d")
 		card.asRawString().should eq("8d")
+	end
+
+	it "Should return full card string correctly" do
+		card = Card.new("Ts")
+		card.asVerboseString().should eq("Ten of Spades")
 	end
 
 end
--- spec/Hand_spec.rb
+++ spec/Hand_spec.rb
@@ -2,9 +2,22 @@ require_relative '../Hand'
 
 describe Hand, "Hand" do
 
-	it "Should import and export raw hand strings correctly" do
-		@hand = Hand.new("8d 5d 2d Jd Kd")
-		hand.asRawString().should eq("8d 5d 2d Jd Kd")
+	it "Should reject hand inputs > 5 cards" do
+		expect{Hand.new("Td 8s 2d 9s 8d 6d")}.to raise_error("HAND PARSE ERROR: number of cards in hand not 5: Td 8s 2d 9s 8d 6d")
 	end
+
+	it "Should reject hand inputs < 5 cards" do
+		expect{Hand.new("2s 7s Qd Jc")}.to raise_error("HAND PARSE ERROR: number of cards in hand not 5: 2s 7s Qd Jc")
+	end
+
+	it "Should import and export raw hand Strings" do
+		hand = Hand.new("8d 8h 2s 3d Kc")
+		hand.asRawString().should eq("8d 8h 2s 3d Kc")
+	end
+
+#	it "Should recognize high card hands" do
+#		hand = Hand.new("9d 7c 5s 3h Kd")
+#		hand.getBestHandType().should eq("High card: King of Diamonds")
+#	end
 
 end
